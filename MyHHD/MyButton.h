7f36311c87529ca0c768df6a9b22ebf034a3ea78
caoye <302995050@qq.com>
2019-05-22 09:40:12 +0800

    add MyTestDemo and MyHHD

---
+++ MyHHD/MyButton.h
@@ -0,0 +1,17 @@
+//
+//  MyButton.h
+//  MyTestDemo
+//
+//  Created by 曹野 on 2019/5/22.
+//  Copyright © 2019 曹野. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MyButton : UIButton
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MyTestDemo/MyTestDemo/MyHHD/MyButton.m
@@ -0,0 +1,21 @@
+//
+//  MyButton.m
+//  MyTestDemo
+//
+//  Created by 曹野 on 2019/5/22.
+//  Copyright © 2019 曹野. All rights reserved.
+//
+
+#import "MyButton.h"
+
+@implementation MyButton
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
